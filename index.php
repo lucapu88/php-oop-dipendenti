0dfabf9e1fc8ce999486bdddd9e702176aef8c7d
lucapu88 <caputoluca88@gmail.com>
2020-02-06 13:17:31 +0100

    inizio

---
+++ index.php
@@ -0,0 +1,6 @@
+<!-- provate a modellizzare le classi per gestire i dipendenti di un'azienda.
+Lascio a voi le scelte architetturali, domani le valutiamo insieme :blush:
+Provate a metterci dentro la gestione di un'eccezione, ad esempio in una funzione che calcola lo stipendio -->
+<?php
+
+ ?>
