ff199334349d0f76e0e3929d6cfc208e80807f67
lucapu88 <caputoluca88@gmail.com>
2020-02-07 16:28:47 +0100

    perfezionato funzioni

--- index.php
+++ index.php
@@ -17,20 +17,14 @@ include_once 'informazioni_comuni.php';
   <?php
     $developer = new Developer('Pippo', 'Franco', 'Javascript', 'Atom'); //creo un nuovo oggetto della classe developer passandogli i parametri obbligatori dichiarati nel costrutttore
     $developer->stampaDipendenti(); //stampo le cose in comune
-    //$developer->setPrintInfo(8, 5, 8);
-    try {
-      $developer->calcolaPagaMensile(8, 5, 8);
-    } catch (Exception $e) {
-      echo 'Eccezione: ' . $e->getMessage();
-    }
-
     $developer->framework = 'bootstrap'; //definisco questa tipologia che non era definita da nessuna parte
     echo '<p>Framework: ' . $developer->framework . '</p>'; //stampo la tipologia definita prima
-    try {
-      echo "RAL: " . $developer->calcolaRal(1400);
-    } catch (Exception $e) {
-      echo 'Eccezione: ' . $e->getMessage();
+    try { //provo la mia funzione
+      $developer->calcolaStipendio(8, 5, 8); //calcola lo stipendio in base a (ore giornaliere,giorni settimanali, paga oraria)
+    } catch (Exception $e) { //nel caso c'è un errore
+      echo 'Eccezione: ' . $e->getMessage(); //in base all'errore recupera il messaggio appropriato e lo stampa
     }
+
   ?>
      <h2>Analisti</h2>
   <?php
@@ -39,10 +33,11 @@ include_once 'informazioni_comuni.php';
      $analista->settore = 'vendite';
      echo '<p>Settore: ' . $analista->settore . '</p>';
      try {
-       echo "RAL: " . $analista->calcolaRal(1800);
+       $analista->calcolaStipendio(9, 5, 11);
      } catch (Exception $e) {
        echo 'Eccezione: ' . $e->getMessage();
      }
+
   ?>
   </body>
 </html>
--- informazioni_comuni.php
+++ informazioni_comuni.php
@@ -6,34 +6,24 @@
     private $oreGiornaliere;
     private $giorniSettimanali;
     private $pagaOraria;
-    //CREA FUNZIONE PER STIPENDIO MENSILE
-      // paga_giornaliera= $pagaOraria per le $oreGiornaliere
-      // paga_settimanale = paga_giornaliera per $giorniSettimanali
-      // return paga_mensile= paga_settimanale per 4
+
     public function setPrintInfo($hour, $day, $pay) { //prende i 3 parametri private li setta e li stampa
       echo '<p>Ore giornaliere: ' . $this->oreGiornaliere = $hour . 'h</p>';
       echo '<p>Giorni Settimanali: ' . $this->giorniSettimanali = $day . 'h</p>';
       echo '<p>Paga oraria: ' . $this->pagaOraria = $pay . '€</p>';
     }
 
-    public function calcolaPagaMensile($hour, $day, $pay) {
-      if (!is_int($hour) && !is_int($day) && !is_int($pay)) { //se i valori passati non sono numeri
+    public function calcolaStipendio($hour, $day, $pay) { //calcola lo stipendio medio mensile
+      if (!is_numeric($hour) || !is_numeric($day) || !is_numeric($pay)) { //se i valori passati non sono numeri
         throw new Exception('Non hai inserito un numero'); //lancia un messaggio di errore
-      } elseif ($hour < 0 && $day < 0 && $pay < 0) { //se i numeri passati sono negativi
+      } elseif ($hour < 0 || $day < 0 || $pay < 0) { //se i numeri passati sono negativi
         throw new Exception('Hai inserito un numero negativo'); //lancia un messaggio di errore
       }
-      $pagaGiornaliera = $pay * $hour;
-      $pagaSettimanale = $pagaGiornaliera * $day;
-      echo '<p>Stipendio mensile: '. $pagaSettimanale * 4 . '€</p>';
-    }
-
-    public function calcolaRal($stipendioMensile) { //funzione a caso creata soltanto come esempio per poter utilizzare una Exception
-      if (!is_int($stipendioMensile)) { //se il valore passato non è un numero
-        throw new Exception('Non hai inserito un numero'); //lancia un messaggio di errore
-      } elseif (empty($stipendioMensile)) { //se invece non viene passato niente
-        throw new Exception("Devi inserire uno stipendio"); //lancia un altro messaggio di error
-      }
-      return $stipendioMensile * 12 . '€'; //la funzione in fine ritorna il calcolo dello stipendio annuale
+      $pagaGiornaliera = $pay * $hour; //calcola la paga giornaliera moltiplicando la paga oraria per le ore giornaliere
+      $pagaSettimanale = $pagaGiornaliera * $day; //calcola la paga settimanale moltiplicando la paga giornaliera calcolata prima per i giorni settimanali
+      $pagaMensile = $pagaSettimanale * 4; //calcola la paga mensile moltiplicando la paga settimanale per 4
+      echo '<p>Stipendio mensile: '. $pagaMensile . '€</p>'; //stampa la paga mensile
+      echo '<p>Stipendio annuale: '. $pagaMensile * 12 . '€</p>'; //stampa il risultato della paga mensile per 12
     }
   }
 
